d19c0ed6bf97e514f2a442c3a883b99d410fb7bb
skumar.jakhr@gmail.com <skumar.jakhr@gmail.com>
2023-06-13 13:37:18 +0100

    drawname

--- example.py
+++ example.py
@@ -20,3 +20,51 @@ def create_and_open_folder(folder_path):
 # Example usage
 folder_path = 'C:/path/to/folder'  # Specify the desired folder path
 create_and_open_folder(folder_path)
+
+
+
+def draw_name(name):
+    special_chars = {
+        'A': '@',
+        'B': '8',
+        'C': '(',
+        'D': '|)',
+        'E': '3',
+        'F': '|=',
+        'G': '6',
+        'H': '|-|',
+        'I': '!',
+        'J': '_|',
+        'K': '|<',
+        'L': '1',
+        'M': '|\\/|',
+        'N': '|\\|',
+        'O': '0',
+        'P': '|D',
+        'Q': '(,)',
+        'R': '|2',
+        'S': '$',
+        'T': '7',
+        'U': '|_|',
+        'V': '\\/',
+        'W': '\\/\\/',
+        'X': '><',
+        'Y': '`/',
+        'Z': '2',
+    }
+
+    name = name.upper()  # Convert the name to uppercase
+
+    for char in name:
+        if char.isalpha():
+            if char in special_chars:
+                print(special_chars[char], end='')
+            else:
+                print(char, end='')
+        else:
+            print(char, end='')
+
+# Example usage
+name = 'John'
+draw_name(name)
+
