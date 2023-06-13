3fab3b23d1e70cd75080400e43648ddd4e7e70b1
skumar.jakhr@gmail.com <skumar.jakhr@gmail.com>
2023-06-13 13:26:04 +0100

    foldercode

--- example.py
+++ example.py
@@ -2,3 +2,21 @@ data = {'Name': ['John', 'Alice', 'Bob'],
         'Age': [25, 30, 35],
         'City': ['New York', 'London', 'Paris']}
 df = pd.DataFrame(data)
+
+
+import os
+import subprocess
+
+def create_and_open_folder(folder_path):
+    # Create the folder
+    os.makedirs(folder_path, exist_ok=True)
+
+    # Open the folder in the file explorer/finder
+    if os.name == 'nt':  # For Windows
+        subprocess.Popen(f'explorer "{folder_path}"')
+    elif os.name == 'posix':  # For macOS/Linux
+        subprocess.Popen(['open', folder_path])
+
+# Example usage
+folder_path = 'C:/path/to/folder'  # Specify the desired folder path
+create_and_open_folder(folder_path)
